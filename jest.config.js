dda214344e7f4206d0bcb208c388680798b80b0e
Ferry Utomo <ferry.utomo@geodan.nl>
2022-10-20 14:47:34 +0200

    Cleanups

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,5 @@
 module.exports = {
     testEnvironment: 'node',
-    testRegex: './tests/.*\\.(ts|tsx)$',
-    testRegex2: './tests/.*\\.(test|spec)?\\.(ts|tsx)$',
+    testRegex: './tests/.*\\.(test|spec)?\\.(ts|tsx)$',
     moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node']
 };
